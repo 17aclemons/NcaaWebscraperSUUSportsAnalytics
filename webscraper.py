379abd719ee68d139aff4e22ee882b38d64df5bf
Andrew Clemons <17aclemons@gmail.com>
2021-01-25 18:04:50 -0700

    gonna use scrapy

--- webscraper.py
+++ webscraper.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
+dl# -*- coding: utf-8 -*-
 """
 Created on Sun Jan 24 20:29:51 2021
 
 @author: User
 """
 
-from bs4 import BeautifulSoup
-import requests
+import scrapy
+
 
 urls = ["https://stats.ncaa.org/team/inst_team_list?academic_year=2020&conf_id=-1&division=1&sport_code=WVB",
           "https://stats.ncaa.org/team/inst_team_list?academic_year=2019&conf_id=-1&division=1&sport_code=WVB",
@@ -28,9 +28,8 @@ urls = ["https://stats.ncaa.org/team/inst_team_list?academic_year=2020&conf_id=-
           "https://stats.ncaa.org/team/inst_team_list?academic_year=2003&conf_id=-1&division=1&sport_code=WVB",
           "https://stats.ncaa.org/team/inst_team_list?academic_year=2002&conf_id=-1&division=1&sport_code=WVB"]
 
-temp = "https://stats.ncaa.org/team/inst_team_list?academic_year=2020&conf_id=-1&division=1&sport_code=WVB"
-test = requests.get(temp)
-years = range(2020,2002)
+
+
 
 
 #pull tags to get url for each team that is d1 for that year
