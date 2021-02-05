2067b21b130ba6e341eac5c5e662e0d10fc4cfc1
Andrew Clemons <17aclemons@gmail.com>
2021-02-04 19:35:08 -0700

    added stuff

--- webscraper.py
+++ webscraper.py
@@ -7,8 +7,9 @@ Created on Sun Jan 24 20:29:51 2021
 
 import scrapy
 
-
-urls = ["https://stats.ncaa.org/team/inst_team_list?academic_year=2020&conf_id=-1&division=1&sport_code=WVB",
+class PostSpider(scrapy.Spider):
+    name = "test"
+    urls = ["https://stats.ncaa.org/team/inst_team_list?academic_year=2020&conf_id=-1&division=1&sport_code=WVB",
           "https://stats.ncaa.org/team/inst_team_list?academic_year=2019&conf_id=-1&division=1&sport_code=WVB",
           "https://stats.ncaa.org/team/inst_team_list?academic_year=2018&conf_id=-1&division=1&sport_code=WVB",
           "https://stats.ncaa.org/team/inst_team_list?academic_year=2017&conf_id=-1&division=1&sport_code=WVB",
@@ -27,13 +28,18 @@ urls = ["https://stats.ncaa.org/team/inst_team_list?academic_year=2020&conf_id=-
           "https://stats.ncaa.org/team/inst_team_list?academic_year=2004&conf_id=-1&division=1&sport_code=WVB",
           "https://stats.ncaa.org/team/inst_team_list?academic_year=2003&conf_id=-1&division=1&sport_code=WVB",
           "https://stats.ncaa.org/team/inst_team_list?academic_year=2002&conf_id=-1&division=1&sport_code=WVB"]
+        
+        
+    start_url = urls[0]
+    
+    
 
-teamUrls = []
-
-
+#gets the team number stuff from response       
+#response.css('a[href*=//team]::attr(href)').getall()    
+        
+        
+        
 #team tags are in a table body, td tag and a tag
-
-
 #pull tags to get url for each team that is d1 for that year
 #pull schedule results for that team
 #get player statistics
