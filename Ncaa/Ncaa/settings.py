9fb1c5ee2ea05f03adb898dd2ebdcdc6819928e9
Andrew Clemons <17aclemons@gmail.com>
2021-03-18 15:34:25 -0600

    Need to create a item pipeline and write to a CSV

--- Ncaa/Ncaa/settings.py
+++ Ncaa/Ncaa/settings.py
@@ -61,9 +61,9 @@ COOKIES_ENABLED = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    'Ncaa.pipelines.NcaaPipeline': 300,
-# }
+ITEM_PIPELINES = {
+    'Ncaa.pipelines.NcaaPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
