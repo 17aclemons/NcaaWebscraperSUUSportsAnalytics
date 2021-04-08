5539d9f4117f069940d86544015e0a04bc8a91d4
Andrew Clemons <17aclemons@gmail.com>
2021-04-08 09:27:21 -0600

    moved code for User use cases

--- Ncaa/Ncaa/spiders/MySpider.py
+++ Ncaa/Ncaa/spiders/MySpider.py
@@ -43,11 +43,39 @@ class NcaaSpider(scrapy.Spider):
         # WVB - Women's Volleyball
 
         # only change these two variables if needed
-        year = ["2020", "2019"]#, "2018", "2017", "2016", "2015", "2014", "2013", "2012",
-               # "2011", "2010", "2009", "2008", "2007", "2006", "2005", "2004", "2003", "2002"]
+        year = [
+            #"2020", 
+            #"2019",
+            #"2018",
+            #"2017",
+            #"2016",
+            #"2015",
+            #"2014",
+            #"2013",
+            #"2012",
+            #"2011",
+            "2010",
+            #"2009",
+            #"2008",
+            #"2007",
+            #"2006",
+            #"2005",
+            #"2004",
+            #"2003",
+            #"2002"
+            ]
         # add or remove sports codes as needed
-        sportCode = ["WBB", "WSB", "WSO", "WTE",
-                    "WLA", "WIH", "WWP", "WSV", "WVB"]
+        sportCode = [
+            "WBB", 
+            "WSB", 
+            "WSO", 
+            "WTE",
+            "WLA", 
+            "WIH", 
+            "WWP", 
+            "WSV", 
+            "WVB"
+            ]
         
         urls = []
         for sport in sportCode:
--- Ncaa/script.bat
+++ Ncaa/script.bat
@@ -1,3 +1,3 @@
-REM use the jl version as the CSV option doesn't get all the data
 scrapy crawl ncaa -O data.json
-REM scrapy crawl ncaa -O data.jl
+py sortAndWrite.py
+
